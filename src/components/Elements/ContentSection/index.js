3a2b4d26029f995c747e42678183146316365f5b
Timo Müller <timo.h.mueller@gmx.de>
2020-08-06 18:26:49 +0200

    adjustments

--- src/components/Elements/ContentSection/index.js
+++ src/components/Elements/ContentSection/index.js
@@ -4,32 +4,30 @@ import "./styles.less";
 
 const ContentSection = (props) => {
   return (
-    <div className={`content-section`}>
-      <div className="container">
-        <Row>
-          {props.orientation === "left" ? (
-            <Fragment>
-              <Col xs={24} md={12} className="image-wrapper">
-                <img width="100%" src={props.image} />
-              </Col>
-              <Col xs={24} md={12} className="text-wrapper">
-                <h2>{props.heading}</h2>
-                {props.text}
-              </Col>
-            </Fragment>
-          ) : (
-            <Fragment>
-              <Col xs={24} md={12} className="text-wrapper">
-                <h2>{props.heading}</h2>
-                {props.text}
-              </Col>
-              <Col xs={24} md={12} className="image-wrapper">
-                <img width="100%" src={props.image} />
-              </Col>
-            </Fragment>
-          )}
-        </Row>
-      </div>
+    <div className={`content-section ${props.orientation}`}>
+      <Row>
+        {props.orientation === "left" ? (
+          <Fragment>
+            <Col xs={24} md={12} className="image-wrapper">
+              <img width="100%" src={props.image} />
+            </Col>
+            <Col xs={24} md={12} className="text-wrapper">
+              <h2>{props.heading}</h2>
+              {props.text}
+            </Col>
+          </Fragment>
+        ) : (
+          <Fragment>
+            <Col xs={24} md={12} className="text-wrapper">
+              <h2>{props.heading}</h2>
+              {props.text}
+            </Col>
+            <Col xs={24} md={12} className="image-wrapper">
+              <img width="100%" src={props.image} />
+            </Col>
+          </Fragment>
+        )}
+      </Row>
     </div>
   );
 };
