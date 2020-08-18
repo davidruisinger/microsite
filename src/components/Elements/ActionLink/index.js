7865962cbcb9017562c030886396e917f7ad30f3
Timo Müller <timo.h.mueller@gmx.de>
2020-08-18 17:05:46 +0200

    finish basic mobile styling

--- src/components/Elements/ActionLink/index.js
+++ src/components/Elements/ActionLink/index.js
@@ -8,10 +8,12 @@ import IconArrowRight from "../../../assets/icons/frame-c-arrow-right.svg";
 const ActionLink = (props) => {
   return (
     <a href={props.link} className="action-link">
-      <Card>
-        <h4>{props.title}</h4>
-        <p>{props.description}</p>
-      </Card>
+      <div>
+        <Card>
+          <h4>{props.title}</h4>
+          <p>{props.description}</p>
+        </Card>
+      </div>
     </a>
   );
 };
