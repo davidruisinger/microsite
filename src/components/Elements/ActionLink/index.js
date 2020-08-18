965dfbae40370fcf5a9927915772a79f37966c72
Timo Müller <timo.h.mueller@gmx.de>
2020-08-18 16:58:17 +0200

    minor styling changes

--- src/components/Elements/ActionLink/index.js
+++ src/components/Elements/ActionLink/index.js
@@ -7,8 +7,8 @@ import IconArrowRight from "../../../assets/icons/frame-c-arrow-right.svg";
 
 const ActionLink = (props) => {
   return (
-    <a href={props.link}>
-      <Card className="action-link">
+    <a href={props.link} className="action-link">
+      <Card>
         <h4>{props.title}</h4>
         <p>{props.description}</p>
       </Card>
