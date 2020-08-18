68a221990bc2f77c5b8d1bad7bb17d8acc3a901e
Timo Müller <timo.h.mueller@gmx.de>
2020-08-18 15:38:57 +0200

    rm magic title

--- src/components/Elements/index.js
+++ src/components/Elements/index.js
@@ -5,7 +5,6 @@ export { default as ArticleCard } from "./ArticleCard";
 export { default as TileLink } from "./TileLink";
 export { default as HeroArticle } from "./HeroArticle";
 export { default as ShareButtons } from "./ShareButtons";
-export { default as MagicTitle } from "./MagicTitle";
 export { default as SuperText } from "./SuperText";
 export { default as TopBar } from "./TopBar";
 export { default as CarbonCounter } from "./CarbonCounter";
