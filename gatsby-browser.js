35a6c81aa9963df88498157bef70b17bb6a4dcea
Timo Müller <timo.h.mueller@gmx.de>
2020-09-09 17:21:57 +0200

    add polyfill and better format the logo

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,11 @@
+import objectFitPolyfill from "objectFitPolyfill";
+
 export const onClientEntry = () => {
   // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
   if (!(`IntersectionObserver` in window)) {
     // eslint-disable-next-line no-unused-expressions
-    import(`intersection-observer`)
-    console.log(`# IntersectionObserver is polyfilled!`)
+    import(`intersection-observer`);
+    console.log(`# IntersectionObserver is polyfilled!`);
+    objectFitPolyfill();
   }
-}
+};
